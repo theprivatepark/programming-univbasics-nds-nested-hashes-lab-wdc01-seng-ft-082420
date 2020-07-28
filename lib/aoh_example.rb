c0d9a68d9384e606c2f0935044609bcd89255fc3
Anney Park <anneypark@gmail.com>
2020-07-28 03:09:50 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,12 +13,12 @@ def assembled_aoh
 end
 
 def literal_aoh
-  [[DON_G],
-  [JOELLE_VD],
-  [PAT_M],
-  [KATE_G],
-  [BRUCE_G]
-  ]
+  [[DON_G,
+  JOELLE_VD,
+  PAT_M,
+  KATE_G,
+  BRUCE_G
+  ]]
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
