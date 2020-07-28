e6b19ffd7b91b537ba394ba2b324b2fccae63604
Anney Park <anneypark@gmail.com>
2020-07-28 02:48:50 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -12,14 +12,17 @@ def assembled_aoh
 end
 
 def literal_aoh
+  {[DON_G],[JOELLE_VD],[PAT_M],[KATE_G],[BRUCE_G]}
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
 
 def aoh_lookup(aoh, row, key)
+  aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
+  aoh[row][key] = new_value
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
